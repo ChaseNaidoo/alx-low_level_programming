d4c751baa75ab963ab18b35a59f53734ec4ba06a
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-16 08:39:51 -0700

    Print Standard Error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (int i = 0; message[i] != '\0'; i++);
-{
-fprintf(stderr, "%c", message[i]);
-}
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
