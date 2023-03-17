604843bcf06bf030a6b73afb63db9106a9853e67
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-17 12:28:46 -0700

    Print lowercase and Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYX";
+
+	for (c = 0; c < 52; c++)
+	putchar(alp[c]);
+	putchar('\n');
+	return (0);
+}
