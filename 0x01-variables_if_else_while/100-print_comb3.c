fa780aa191ae3dd9092887fc6a2164785fb3e8fb
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-17 16:37:03 -0700

    Two Digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first < 9; first++)
+	{
+	for (second = first + 1; second < 10; second++)
+	{
+        putchar((first % 10) + '0');
+	putchar((second % 10) + '0');
+	if (first == 8 && second == 9)
+	  continue;
+	  	putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
