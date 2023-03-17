c535966291ca1ee8c4ae034b10298d780ee4fa70
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-17 14:52:10 -0700

    Print Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
+	{
 	putchar(n + '0');
 	if (n < 9)
-
-	putchar(',');
-	putchar(' ');
-
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
