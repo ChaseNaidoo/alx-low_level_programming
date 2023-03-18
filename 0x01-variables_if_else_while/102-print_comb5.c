909e2ec721652c8a036cf9234dc02b16a26f0d4f
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-17 17:44:17 -0700

    Two Two combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 	{
 	for (second = first + 1; second < 99; second++)
 	{
-	putchar((first / 10) + '0');
-	putchar((first % 10) + '0');
+	putchar(first / 10) + '0');
+	putchar(first % 10) + '0');
 	putchar(' ');
-	putchar((second / 10) + '0');
-	putchar((second % 10) + '0');
+	putchar(second / 10) + '0');
+	putchar(second % 10) + '0');
 	if (first == 98 && second == 99)
 	continue;
 		putchar(',');
