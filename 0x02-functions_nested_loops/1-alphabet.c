3dfb67035573f854c54e1d318f9088af33a29873
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-20 06:54:42 -0700

    Print Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-
-int main(int argc, char *args[])
-{
-	print_alphabet();
-	return (0);
-}
-void print_alphabet
+void print_alphabet(void)
 {
 	int _putchar(char c);
 	char *str = "abcdefghijklmnopqrstuvwxyz\n";
