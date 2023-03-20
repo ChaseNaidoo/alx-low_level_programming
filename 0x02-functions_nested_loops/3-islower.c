97930aaa5068baf0adba6a2f18dba5a67108530f
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-20 15:34:22 -0700

    Check Lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,21 @@
 /**
  * _islower - Entry point
  *
- * checks whether the ASCII value of c is between 97 and 122, inclusive.
+ * @c: the character to check
+ *
+ * checks whether the value of c is a and z, inclusive.
  *
  * Return: 1 (lowercase), Return: 0 (otherwise)
+ *
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 {
 	return (1);
 }
 	else
-{
+{	  
 	return (0);
 }
-	_putchar('\n');
 }
