1bddd0fec467826913269b273089d359684aaa53
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-20 06:16:19 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _putchar(char c);
+	char *str = "_putchar\n";
+
+	for (int i = 0; str[i] != '\0'; i++)
+{
+	_putchar(str[i]);
+}
+	return (0)
+}
