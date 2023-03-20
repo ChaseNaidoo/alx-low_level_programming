89f02440aa2bbdd6bb790851ab7f33ed7b20ea6e
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-20 15:01:06 -0700

    Print Lowercase x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 {
 	int i, c;
 
-	for (c = 0; c < 10; c++)
+	for (i = 0; i < 10; i++)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < 26; i++)
+	for (c = 0; c < 26; c++)
 	_putchar(alp[c]);
 	_putchar('\n');
 }
