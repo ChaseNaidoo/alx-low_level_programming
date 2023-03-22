5813c03f73374449425db062eb9a095550c7f330
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-21 18:38:03 -0700

    Print and Find Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	int counter = 2;
+	const int MOD = 1000000000;
 
-	unsigned long int a = 1, b = a + 1, c = (a + b) % 4294967296UL;
-
+	long int a = 1, b = a + 1, c = (a + b);
+	
 	printf("%lu, %lu, ", a, b);
 
 	while (counter < 98)
 {
-	printf("%lu", c);
+	printf("%lu", c % MOD);
 	a = b;
 	b = c;
-	c = (a + b) % 4294967296UL;
+	c = (a + b) % MOD;
 	counter++;
 	if (counter < 98)
 {
