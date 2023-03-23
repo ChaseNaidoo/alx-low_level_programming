6cfa538a021732413a48d1319e4ad23baad09da4
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-23 09:05:48 -0700

    Multiply Integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: (a * b)
+ */
+	int mul(int a, int b)
+{
+	return (a * b);
+}
