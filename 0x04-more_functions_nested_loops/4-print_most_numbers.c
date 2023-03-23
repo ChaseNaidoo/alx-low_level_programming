36736988d5390b715c86a656af62f0db61e14ac2
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-23 09:25:36 -0700

    Print numbers 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Void (Success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+	char num[8] = "01356789";
+
+	for (i = 0; i < 8; i++)
+	_putchar(num[i]);
+	_putchar('\n');
+}
