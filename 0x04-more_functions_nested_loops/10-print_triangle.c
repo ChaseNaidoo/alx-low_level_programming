4f876178bf53b735ac0824727ca308568707558b
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-23 16:19:41 -0700

    Print Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ *@size: is the number of #\## printed
+ *
+ * Return: '#\##', '\n'
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+{
+	for (b = 1; b < (size - a); b++)
+	_putchar(' ');
+	for (b--; b < size; b++)
+	_putchar('#');
+	if (a < (size - 1))
+	_putchar('\n');
+}
+	_putchar('\n');
+}
