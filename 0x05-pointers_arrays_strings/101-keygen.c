9a3eb4956dff161c15cf17a49327ce4406bad475
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-27 20:00:45 -0700

    Passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0, b = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+{
+	r = rand() % 128;
+	if ((a + b) > 2772)
+{
+	b = b + a;
+}
+	printf("%c", a);
+}
+	printf("%c\n", (2772 - b));
+	return (0);
+}
