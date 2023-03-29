bf317fced68cda6a778dc13315a4433b306fb6de
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-29 14:04:19 -0700

    Capitalize String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,12 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i, a;
 	int cap = 1;
+	char sep[] = {
+	' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'
+	};
+	int n = sizeof(sep) / sizeof(sep[0]);
 
 	for (i = 0; str[i] != '\0'; i++)
 {
@@ -19,13 +23,20 @@ char *cap_string(char *str)
 	str[i] = str[i] - 32;
 	cap = 0;
 }
-	else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||  str[i] == ';' ||  str[i] == '.' ||  str[i] == '!' ||  str[i] == '?' ||  str[i] == '"' ||  str[i] == '(' ||  str[i] == ')' ||  str[i] == '{' ||  str[i] == '}')
-{
-	cap = 1;
-}
 	else
 {
-	    cap = 0;
+	for (a = 0; a < n; a++)
+{
+	if (str[i] == sep[a])
+{
+	cap = 1;
+	break;
+}
+}
+	if (a == n)
+{
+	cap = 0;
+}
 }
 }
 	return (str);
