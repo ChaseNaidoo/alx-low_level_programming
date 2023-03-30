51c60ef8a6ebad83fe41b75afc43719c4d451df9
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-03-30 11:31:21 -0700

    rot13 code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,37 +1,29 @@
 #include "main.h"
 
 /**
- * rot13 - entry point
- *
- * @str: input characters
- *
- * Return: (str)
- */
+* rot13 - entry point
+*
+* @str: input characters
+*
+* Return: (str)
+*/
 char *rot13(char *str)
 {
-	char c;
-	int i;
+	int i, a;
+	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str && str[i]; i++)
 {
-	c = str[i];
-	if (c >= 'a' && c <= 'm')
+	a = 0;
+	while (alp[a] != '\0' && alp[a] != str[i])
 {
-	c += 13;
+	a++;
 }
-	else if (c >= 'n' && c <= 'z')
+	if (alp[a] == str[i])
 {
-	c -= 13;
+	str[i] = rot[a];
 }
-	else if (c >= 'A' && c <= 'M')
-{
-	c += 13;
-}
-	else if (c >= 'N' && c <= 'Z')
-{
-	c -= 13;
-}
-	str[i] = c;
 }
 	return (str);
 }
