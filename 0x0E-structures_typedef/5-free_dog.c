68b670ace2b863f169217d17fb6b388fedb516ee
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-17 12:25:12 -0700

    Free Dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free memory allocated for struct dog.
+ *
+ * @d: first parameter
+ *
+ * Return: (no return)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
