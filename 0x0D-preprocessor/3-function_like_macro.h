c8c336398bb7b6d935775ea907310bd791e2da5e
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-17 13:38:04 -0700

    Compute Value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef function_like_macro_h
-#define function_like_macro_h
+#ifndef _function_like_macro_h
+#define _function_like_macro_h
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 
 #endif
