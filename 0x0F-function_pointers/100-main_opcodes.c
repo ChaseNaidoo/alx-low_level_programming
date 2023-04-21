965ad8d384bdf65d916a8b971ca23cb33503271b
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-21 12:11:37 -0700

    Print Own main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 /**
  * main - entry point
+ *
  * @argc: argument count
  * @argv: argument vector
  *
- * Return 0 (success)
+ * Return: 0 (success)
  *
  */
 int main(int argc, char *argv[])
