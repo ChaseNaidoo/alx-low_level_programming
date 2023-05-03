7d8f30305fdbf37d7ea300b423dd7e01e8028fc3
ChaseNaidoo <chasenaidoo9@gmail.com>
2023-05-03 12:43:30 -0700

    Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->next = node->next;
-	new_node = node->next;
+	node->next = new_node;
 
 	return (new_node);
 }
