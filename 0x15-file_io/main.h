f60969dbf6e60ab2b4d9cf0b27f3bd9f66b5ea9e
root <chasenaidoo9@gmail.com>
2023-07-10 05:12:59 -0700

    Prints POSIX

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef _main_h
 #define _main_h
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <elf.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
